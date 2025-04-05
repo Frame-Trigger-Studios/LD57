ad0b938022ea07c71a25fd01cfd07de051fe61c3
Dylan <dylanljenkins@gmail.com>
2025-04-05 15:48:45 +1100

    12x12

--- src/DemoThings.ts
+++ src/DemoThings.ts
@@ -14,7 +14,7 @@ export class DemoThings extends Entity
         for (let i = 0; i < 20; i++)
         {
             let e = this.scene.addEntity(new Entity("space junk", MathUtil.randomRange(0, 512), MathUtil.randomRange(512, 1024)));
-            e.addComponent(new RenderRect(0, 0, 16, 16, 0xFF1100));
+            e.addComponent(new RenderRect(0, 0, 12, 12, 0xFF1100));
             e.addComponent(new MovingThing());
         }
 
